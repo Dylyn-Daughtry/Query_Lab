a71409129cf68f8d3a78d4cab000ca09be714f62
Dylyn-Daughtry <98605975+Dylyn-Daughtry@users.noreply.github.com>
2022-02-13 11:38:10 -0500

    Problems 6-7 Completed

--- school_db/views.py
+++ school_db/views.py
@@ -279,7 +279,9 @@ VALUES ('Kyle', 'Harwood', 2022, 3.0)
 def problem_six(request):
     
     # Make sure to set this equal to the primary key of the row you just created!
-    student_id = 11
+    Student.objects.filter(id=11).update(gpa=4.0)
+    updated_student = Student.objects.get(pk=11)
+    print(f'Name:{updated_student.first_name} {updated_student.last_name} Grade:{updated_student.year} GPA:{updated_student.gpa}')
 
 
 
@@ -328,13 +330,13 @@ LIMIT 21
 def problem_seven(request):
 
     # Make sure to set this equal to the primary key of the row you just created!
-    student_id = 11
+    Student.objects.filter(id=11).delete()
 
 
-    try:
-        student = Student.objects.get(pk=student_id)
-    except ObjectDoesNotExist:
-        print('Great! It failed and couldnt find the object because we deleted it!')
+    # try:
+    #     student = Student.objects.get(pk=11)
+    # except ObjectDoesNotExist:
+    #     print('Great! It failed and couldnt find the object because we deleted it!')
 
     return complete(request)
 
